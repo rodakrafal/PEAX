4d3308a363822231d06528b770f6bb0793faca64
m <>
2023-12-13 18:34:17 +0100

    Add basic logic for menu and CSV files
    Add menu that lets you decide what to pick
    Add option to generate items and save them to CSV file
    Add option to load items from CSV file
    
    Signed-off-by: mcytrycki

--- item_generator.py
+++ item_generator.py
@@ -1,3 +1,4 @@
+import csv
 from dataclasses import dataclass
 from random import randint
 from typing import List
@@ -22,3 +23,19 @@ class ItemGenerator:
             items.append(Item(width, height, id=str(i)))
 
         return items
+    def load_items(self, file_path: str) -> List[Item]:
+        items = []
+
+        with open(file_path, 'r') as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for i, row in enumerate(csv_reader):
+                width, height = map(int, row)
+                if 0 < width <= self.bin_width and 0 < height <= self.bin_height:
+                    items.append(Item(width, height, id=str(i)))
+        return items
+    def save_items(self, file_path: str, items: List[Item]) -> None:
+        with open(file_path, 'w', newline='') as csv_file:
+            csv_writer = csv.writer(csv_file)
+            for item in items:
+                if 0 < item.width <= self.bin_width and 0 < item.height <= self.bin_height:
+                    csv_writer.writerow([item.width, item.height])
